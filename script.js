4b8605eba66f330a0e2bc36305fc1b23634a9459
rashedul <rashedul@outlook.com>
2024-07-12 13:19:47 +0100

    3 box example

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ yellowBtn.addEventListener('mouseover', function() {
     buildingBody.style.backgroundColor = 'yellow';
 });
 
+yellowBtn.addEventListener('click', function() {
+    const doorKnob = document.getElementsByClassName('door-knob');
+    // doorKnob[0].style.width = '50px';
+    // doorKnob[0].style.height = '50px';
+
+    for (let i = 0; i < doorKnob.length; i++) {
+        doorKnob[i].style.width = '50px';
+        doorKnob[i].style.height = '50px';
+    }
+});
+
+
 greenBtn.addEventListener('mouseover', function() {
     buildingBody.style.backgroundColor = 'green';
 });
